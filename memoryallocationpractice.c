17e22e5ba4ad09feb8c58bdbfb0b8b4fddfeb82e
kamilshan <kamiljavaid24@gmail.com>
2025-12-07 11:40:20 +0530

    added practice file

---
+++ memoryallocationpractice.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+// this is just a practice program for dynamic memmory allocation and reallocation
+
+
+int main() {
+  int *ptr1, *ptr2, size;
+
+  // Allocate memory for four integers
+  size = 4 * sizeof(*ptr1);
+  ptr1 = malloc(size);
+
+  printf("%d bytes allocated at address %p \n", size, ptr1);
+
+  // Resize the memory to hold six integers
+  size = 6 * sizeof(*ptr1);
+  ptr2 = realloc(ptr1, size);
+
+  printf("%d bytes reallocated at address %p \n", size, ptr2);
+ 
+  return 0;
+}
